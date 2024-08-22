e4cdda6c85ee7566fdd51905f4465329b8970ca3
jacky <jacky.le.goldenowl@gmail.com>
2024-08-22 14:57:33 +0700

    chore: F

--- lib/generators/view_component/view_component_generator.rb
+++ lib/generators/view_component/view_component_generator.rb
@@ -2,55 +2,60 @@
 
 require 'rails/generators'
 # Based on https://github.com/github/view_component/blob/master/lib/rails/generators/component/component_generator.rb
-class ViewComponentGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('templates', __dir__)
+module Generators
+  module ViewComponent
+    class ViewComponentGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('templates', __dir__)
 
-  class_option :skip_test, type: :boolean, default: false
-  class_option :skip_system_test, type: :boolean, default: false
-  class_option :skip_preview, type: :boolean, default: false
+      class_option :skip_test, type: :boolean, default: false
+      class_option :skip_system_test, type: :boolean, default: false
+      class_option :skip_preview, type: :boolean, default: false
 
-  argument :attributes, type: :array, default: [], banner: 'attribute'
+      argument :attributes, type: :array, default: [], banner: 'attribute'
 
-  def create_component_file
-    template 'component.rb', File.join('app/frontend/components', class_path, file_name, 'component.rb')
-  end
+      def create_component_file
+        template 'component.rb', File.join('app/frontend/components', class_path, file_name, 'component.rb')
+      end
 
-  def create_template_file
-    template 'component.html.slim', File.join('app/frontend/components', class_path, file_name, 'component.html.slim')
-  end
+      def create_template_file
+        template 'component.html.slim',
+          File.join('app/frontend/components', class_path, file_name, 'component.html.slim')
+      end
 
-  def create_test_file
-    return if options[:skip_test]
+      def create_test_file
+        return if options[:skip_test]
 
-    template 'component_spec.rb', File.join('spec/frontend/components', class_path, "#{file_name}_spec.rb")
-  end
+        template 'component_spec.rb', File.join('spec/frontend/components', class_path, "#{file_name}_spec.rb")
+      end
 
-  # def create_system_test_file
-  #   return if options[:skip_system_test]
+      # def create_system_test_file
+      #   return if options[:skip_system_test]
 
-  #   template 'component_system_spec.rb',
-  #     File.join('spec/system/frontend/components', class_path, "#{file_name}_spec.rb")
-  # end
+      #   template 'component_system_spec.rb',
+      #     File.join('spec/system/frontend/components', class_path, "#{file_name}_spec.rb")
+      # end
 
-  def create_preview_file
-    return if options[:skip_preview]
+      def create_preview_file
+        return if options[:skip_preview]
 
-    template 'preview.rb', File.join('app/frontend/components', class_path, file_name, 'preview.rb')
-  end
+        template 'preview.rb', File.join('app/frontend/components', class_path, file_name, 'preview.rb')
+      end
 
-  private
+      private
 
-  def parent_class
-    'ApplicationViewComponent'
-  end
+      def parent_class
+        'ApplicationViewComponent'
+      end
 
-  def preview_parent_class
-    'ApplicationViewComponentPreview'
-  end
+      def preview_parent_class
+        'ApplicationViewComponentPreview'
+      end
 
-  def initialize_signature
-    return if attributes.blank?
+      def initialize_signature
+        return if attributes.blank?
 
-    attributes.map { |attr| "option :#{attr.name}" }.join("\n  ")
+        attributes.map { |attr| "option :#{attr.name}" }.join("\n  ")
+      end
+    end
   end
 end
