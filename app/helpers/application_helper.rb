c290166dd304a775917ca0cc734d2d98ef35eeed
jacky <jacky.le.goldenowl@gmail.com>
2024-08-27 17:32:32 +0700

    feat: form fields component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  ActionView::Base.default_form_builder = FormBuilders::DaisyFormBuilder
+
   def component(name, *, **, &)
     component = name.to_s.camelize.constantize::Component
     render(component.new(*, **), &)
